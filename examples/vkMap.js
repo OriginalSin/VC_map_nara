21df314905a878ccf99a5872f0113aa811b8683c
OriginalSin <saleks@scanex.ru>
2015-06-17 14:06:05 +0300

    tt

--- examples/vkMap.js
+++ examples/vkMap.js
@@ -158,7 +158,7 @@ L.vkMap = function(cont, options) {
                 photoUtils.curNum = num;
                 if (it.player) {
                     var iframe = L.DomUtil.create('iframe', 'video', div);
-                    iframe.setAttribute('width', 607);
+                    iframe.setAttribute('width', 400);
                     iframe.setAttribute('height', 360);
                     iframe.setAttribute('frameborder', 0);
                     iframe.src = it.player.replace(/http:/, 'https:') + '&hd=max';
